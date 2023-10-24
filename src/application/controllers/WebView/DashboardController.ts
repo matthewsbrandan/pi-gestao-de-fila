7dad0fbf7983f9a3318bbf30ed869b002d7fb4b4
Mateus Brandão <mateusfleria@gmail.com>
2023-10-24 01:03:57 -0300

    feat: Montando estrutura do front da dashbiard

--- src/application/controllers/WebView/DashboardController.ts
+++ src/application/controllers/WebView/DashboardController.ts
@@ -14,6 +14,10 @@ export class DashboardController extends Controller{
       
       const data = await this.useCase.execute();
 
+      return response.json({
+        data
+      })
+      
       return this.view('dashboard/index.ejs', { data })
     } catch (error) {
       this.notify('error', error.message)
